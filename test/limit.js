fb352ec7eeacf2249eefef30cc755b06d8bc2c57
Rebecca Turner <me@re-becca.org>
2015-09-17 23:50:17 +0000

    make tests more deterministic

--- test/limit.js
+++ test/limit.js
@@ -4,66 +4,96 @@ var limit = require('../index.js')
 
 
 test('limit', function (t) {
-  t.plan(4)
+  t.plan(5)
   var called = 0
   var completed = 0
+  var finishers = []
   var limited = limit(function (num, cb) {
     ++ called
-    setTimeout(cb, 10)
+    finishers.push(cb)
   }, 2)
   ;[1,2,3].forEach(function (num) { limited(num, function () { ++ completed }) })
   setImmediate(function () {
     t.is(called, 2, 'Immediately queued 2 callbacks')
     t.is(completed, 0, 'With no completion yet')
+    // immediately complete one of them...
+    finishers.shift()()
+    setImmediate(afterCompletion)
   })
-  setTimeout(function () {
-    t.is(called, 3, 'And ran final callback after waiting')
-    t.is(completed, 2, 'And two complete')
-  }, 11)
+  function afterCompletion () {
+    t.is(completed, 1, 'Calling the finisher completed the first one')
+    t.is(called, 3,'Third action was started')
+    finishers.shift()()
+    finishers.shift()()
+    setImmediate(afterAllDone)
+  }
+  function afterAllDone () {
+    t.is(completed, 3, 'All completed')
+  }
 })
 
 test('limit-obj-method', function (t) {
-  t.plan(4)
+  t.plan(5)
   var called = 0
   var completed = 0
+  var finishers = []
   var example = {
-    sleep: function (num, cb) {
+    test: function (num, cb) {
       ++ called
-      setTimeout(cb, 10)
+      finishers.push(cb)
     }
   }
-  limit.method(example, 'sleep', 2)
-  ;[1,2,3].forEach(function (num) { example.sleep(num, function () { ++ completed }) })
+  limit.method(example, 'test', 2)
+  ;[1,2,3].forEach(function (num) { example.test(num, function () { ++ completed }) })
   setImmediate(function () {
     t.is(called, 2, 'Immediately queued 2 callbacks')
     t.is(completed, 0, 'With no completion yet')
+    // immediately complete one of them...
+    finishers.shift()()
+    setImmediate(afterCompletion)
   })
-  setTimeout(function () {
-    t.is(called, 3, 'And ran final callback after waiting')
-    t.is(completed, 2, 'And two complete')
-  }, 11)
+  function afterCompletion () {
+    t.is(completed, 1, 'Calling the finisher completed the first one')
+    t.is(called, 3,'Third action was started')
+    finishers.shift()()
+    finishers.shift()()
+    setImmediate(afterAllDone)
+  }
+  function afterAllDone () {
+    t.is(completed, 3, 'All completed')
+  }
 })
 
 test('limit-class-method', function (t) {
-  t.plan(4)
+  t.plan(5)
   var called = 0
   var completed = 0
+  var finishers = []
   function Example () {}
   Example.prototype = {
-    sleep: function (num, cb) {
+    test: function (num, cb) {
       ++ called
-      setTimeout(cb, 10)
+      finishers.push(cb)
     }
   }
-  limit.method(Example, 'sleep', 2)
+  limit.method(Example, 'test', 2)
   var example = new Example()
-  ;[1,2,3].forEach(function (num) { example.sleep(num, function () { ++ completed }) })
+  ;[1,2,3].forEach(function (num) { example.test(num, function () { ++ completed }) })
   setImmediate(function () {
     t.is(called, 2, 'Immediately queued 2 callbacks')
     t.is(completed, 0, 'With no completion yet')
+    // immediately complete one of them...
+    finishers.shift()()
+    setImmediate(afterCompletion)
   })
-  setTimeout(function () {
-    t.is(called, 3, 'And ran final callback after waiting')
-    t.is(completed, 2, 'And two complete')
-  }, 11)
+  function afterCompletion () {
+    t.is(completed, 1, 'Calling the finisher completed the first one')
+    t.is(called, 3,'Third action was started')
+    finishers.shift()()
+    finishers.shift()()
+    setImmediate(afterAllDone)
+  }
+  function afterAllDone () {
+    t.is(completed, 3, 'All completed')
+  }
 })
