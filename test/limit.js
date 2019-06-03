d5069f4c3f11615987946bd65918c8a0533b359d
Rebecca Turner <me@re-becca.org>
2019-06-02 19:16:59 -0700

    test: Test coverage for limited promise based methods

--- test/limit.js
+++ test/limit.js
@@ -133,3 +133,81 @@ test('promise', function (t) {
     t.is(completed, 3, 'All completed')
   }
 })
+
+test('limit-promiseobj-method', function (t) {
+  t.plan(5)
+  var called = 0
+  var completed = 0
+  var finishers = []
+  var example = {
+    test: function (num) {
+      ++ called
+      return new Promise(resolve => {
+        finishers.push(resolve)
+      })
+    }
+  }
+  limit.promise.method(example, 'test', 2)
+  for (var ii = 0; ii < 3; ++ii) {
+    example.test(ii + 1).then(function () {
+      ++ completed
+    })
+  }
+  setImmediate(function () {
+    t.is(called, 2, 'Immediately queued 2 callbacks')
+    t.is(completed, 0, 'With no completion yet')
+    // immediately complete one of them...
+    finishers.shift()()
+    setImmediate(afterCompletion)
+  })
+  function afterCompletion () {
+    t.is(completed, 1, 'Calling the finisher completed the first one')
+    t.is(called, 3,'Third action was started')
+    finishers.shift()()
+    finishers.shift()()
+    setImmediate(afterAllDone)
+  }
+  function afterAllDone () {
+    t.is(completed, 3, 'All completed')
+  }
+})
+
+test('limit-promiseclass-method', function (t) {
+  t.plan(5)
+  var called = 0
+  var completed = 0
+  var finishers = []
+  function Example () {}
+  Example.prototype = {
+    test: function (num, cb) {
+      ++ called
+      return new Promise(resolve => {
+        finishers.push(resolve)
+      })
+    }
+  }
+  limit.promise.method(Example, 'test', 2)
+  var example = new Example()
+  for (var ii = 0; ii < 3; ++ii) {
+    example.test(ii + 1).then(function () {
+      ++ completed
+    })
+  }
+  setImmediate(function () {
+    t.is(called, 2, 'Immediately queued 2 callbacks')
+    t.is(completed, 0, 'With no completion yet')
+    // immediately complete one of them...
+    finishers.shift()()
+    setImmediate(afterCompletion)
+  })
+  function afterCompletion () {
+    t.is(completed, 1, 'Calling the finisher completed the first one')
+    t.is(called, 3,'Third action was started')
+    finishers.shift()()
+    finishers.shift()()
+    setImmediate(afterAllDone)
+  }
+  function afterAllDone () {
+    t.is(completed, 3, 'All completed')
+  }
+})
